6f510a7cb31ab4ad8a34f7e659f473ec16bbc4aa
Kalita, Matthew H <mkalita6@gatech.edu>
2021-02-21 14:12:12 -0500

    update images and code for full run

--- ml_mhk_file.py
+++ ml_mhk_file.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
@@ -9,7 +10,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import learning_curve
-
+import time
 # define data filenames
 data_filenames = [
     'winequality-red.csv',
@@ -126,43 +127,121 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 # Prediction Models
 # store the scores of the models
-scores_train = {}
+scores_train_times = {}
 scores_test = {}
 
 # Decision Tree wine
 dtree = DecisionTreeClassifier()
+start = time.time()
 dtree.fit(x_train, y_train)  # train model
+stop = time.time()
+print(f" Dtree Training time: {stop - start}s")
+scores_train_times['DT'] =  {stop - start}
 scores_test['DT'] = dtree.score(x_test, y_test)  # score model
+####   below sample of trees depth and acuracy 1-30
+max_depth = []
+acc_gini = []
+acc_entropy = []
+for i in range(1, 30):
+    dtree = DecisionTreeClassifier(criterion='gini', max_depth=i)
+    dtree.fit(x_train, y_train)
+    pred = dtree.predict(x_test)
+    acc_gini.append(accuracy_score(y_test, pred))
+    dtree = DecisionTreeClassifier(criterion='entropy', max_depth=i)
+    dtree.fit(x_train, y_train)
+    pred = dtree.predict(x_test)
+    acc_entropy.append(accuracy_score(y_test, pred))
+    max_depth.append(i)
+
+d = pd.DataFrame({'acc_gini' :pd.Series(acc_gini),
+'acc_entropy':pd.Series(acc_entropy),
+ 'max_depth':pd.Series(max_depth)})
+# visualizing changes in parameters
+
+
+# print(d)
+plt.plot(max_depth, acc_gini, data=None, label='gini')
+plt.plot(max_depth, acc_entropy, data=None, label='entropy')
+plt.xlabel('max_depth')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
+# print(max_depth)
+# print(acc_gini)
+# print(acc_entropy)
+# exit()
 
 # Neural Networks
 # Multi-Layer Perceptron Classifier
-mlp = MLPClassifier()
+mlp = MLPClassifier(hidden_layer_sizes=(8,8,8),max_iter=300)
+start = time.time()
 mlp.fit(x_train, y_train)  # train model
+stop = time.time()
+print(f"NN Training time: {stop - start}s")
+scores_train_times['NN'] =  {stop - start}
 scores_test['MLP'] = mlp.score(x_test, y_test)  # score model
 
 # Boosted Decision Tree
 # Gradient Boosting Classifier
 gboost = GradientBoostingClassifier()
+start = time.time()
 gboost.fit(x_train, y_train)  # train model
+stop = time.time()
+print(f" Boost Training time: {stop - start}s")
+scores_train_times['Boost'] ={stop - start}
 scores_test['Boosting'] = gboost.score(x_test, y_test)  # score model
 
 # Support Vector Machines
 svm = SVC()
+start = time.time()
 svm.fit(x_train, y_train)  # train model
+stop = time.time()
+print(f" svc Training time: {stop - start}s")
+scores_train_times['sv'] =  {stop - start}
 scores_test['SVM'] = svm.score(x_test, y_test)  # score model
 
-# k-Nearest Neighbors
-knn = KNeighborsClassifier()
+# k-Nearest Neighbors   # default is 5
+knn = KNeighborsClassifier(n_neighbors=5)
+start = time.time()
 knn.fit(x_train, y_train)  # train model
+stop = time.time()
+print(f" knn Training time: {stop - start}s")
+scores_train_times['knn'] =  {stop - start}
 scores_test['k-NN'] = knn.score(x_test, y_test)  # score model
+
+# k-Nearest Neighbors   # 3
+knn_3 = KNeighborsClassifier(n_neighbors=3)
+start = time.time()
+knn_3.fit(x_train, y_train)  # train model
+stop = time.time()
+print(f" knn_3 Training time: {stop - start}s")
+scores_train_times['knn_3'] =  {stop - start}
+scores_test['k-NN_3'] = knn_3.score(x_test, y_test)  # score model
+
+# k-Nearest Neighbors   # 10
+knn_10 = KNeighborsClassifier(n_neighbors=10)
+start = time.time()
+knn_10.fit(x_train, y_train)  # train model
+stop = time.time()
+print(f" knn_10 Training time: {stop - start}s")
+scores_train_times['knn_10'] =  {stop - start}
+scores_test['k-NN_10'] = knn_3.score(x_test, y_test)  # score model
+
 
 # output scores
 print(scores_test)
+# print(scores_train_times)
 
+###################  DTREE ######################################################
+# comment out for different depth
+# dtree = DecisionTreeClassifier(max_depth=7)
+start = time.time()
 train_sizes, train_scores, test_scores = learning_curve(dtree, x_train, y_train, n_jobs=-1, cv=5,
                                                         train_sizes=np.linspace(.1, 1.0, 5),
                                                         verbose=0)
-
+stop = time.time()
+print(f" DT CV Classifier: {stop - start}s")
+scores_train_times['DT CV Classifier'] = {stop - start}
 train_scores_mean = np.mean(train_scores, axis=1)
 train_scores_std = np.std(train_scores, axis=1)
 test_scores_mean = np.mean(test_scores, axis=1)
@@ -192,6 +271,177 @@ plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation
 plt.ylim(-.1, 1.1)
 plt.show()
 
+################################################################################
+# exit()
+################### KNN ##################
+knn_list= {knn,knn_3 ,knn_10}
+
+
+for i in knn_list:
+    train_sizes, train_scores, test_scores = learning_curve(i, x_train, y_train, n_jobs=-1, cv=10,
+                                                            train_sizes=np.linspace(.1, 1.0, 5),
+                                                            verbose=0)
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    plt.figure()
+
+    plt.title("KNN 5 ,3,10 ")
+    plt.legend(loc="best")
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    plt.gca().invert_yaxis()
+    # box-like grid
+    plt.grid()
+
+    # plot the std deviation as a transparent range at each training set size
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
+                     color="g")
+
+    # plot the average training and test score lines at each training set size
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+
+    # sizes the window for readability and displays the plot
+    # shows error from 0 to 1.1
+    plt.ylim(-.1, 1.1)
+    plt.show()
+
+
+
+#########################################################
+##########  NURAL NETWORK #################
+
+# mlp = MLPClassifier(hidden_layer_sizes=(8,8,8),max_iter=300)
+start = time.time()
+train_sizes, train_scores, test_scores = learning_curve(mlp, x_train, y_train, n_jobs=-1, cv=10,
+                                                        train_sizes=np.linspace(.1, 1.0, 5),
+                                                        verbose=0)
+stop = time.time()
+print(f" NN MLPClassifier: {stop - start}s")
+scores_train_times['NN MLPClassifier'] = {stop - start}
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
+
+plt.figure()
+plt.title("Multi-Layer Perceptron Classifier")
+plt.legend(loc="best")
+plt.xlabel("Training examples")
+plt.ylabel("Score")
+plt.gca().invert_yaxis()
+# box-like grid
+plt.grid()
+
+# plot the std deviation as a transparent range at each training set size
+plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1,
+                 color="r")
+plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
+                 color="g")
+
+# plot the average training and test score lines at each training set size
+plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+
+# sizes the window for readability and displays the plot
+# shows error from 0 to 1.1
+plt.ylim(-.1, 1.1)
+plt.show()
+
+
+##################################################
+##########  boost #################
+start = time.time()
+train_sizes, train_scores, test_scores = learning_curve(gboost, x_train, y_train, n_jobs=-1, cv=10,
+                                                        train_sizes=np.linspace(.1, 1.0, 5),
+                                                        verbose=0)
+stop = time.time()
+print(f" GBOOST: {stop - start}s")
+scores_train_times['GBOOST'] = {stop - start}
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
+
+plt.figure()
+plt.title("GradientBoostingClassifier")
+plt.legend(loc="best")
+plt.xlabel("Training examples")
+plt.ylabel("Score")
+plt.gca().invert_yaxis()
+# box-like grid
+plt.grid()
+
+# plot the std deviation as a transparent range at each training set size
+plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1,
+                 color="r")
+plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
+                 color="g")
+
+# plot the average training and test score lines at each training set size
+plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+
+# sizes the window for readability and displays the plot
+# shows error from 0 to 1.1
+plt.ylim(-.1, 1.1)
+plt.show()
+
+
+##################################################
+
+################### svm ##################
+svm_list= {'linear', 'poly', 'rbf', 'sigmoid'}
+
+
+for i in svm_list:
+    start = time.time()
+    train_sizes, train_scores, test_scores = learning_curve(SVC(kernel=i), x_train, y_train, n_jobs=-1, cv=10,
+                                                            train_sizes=np.linspace(.1, 1.0, 5),
+                                                            verbose=0)
+    stop = time.time()
+    print(f" 'linear', 'poly', 'rbf', 'sigmoid': {stop - start}s")
+    scores_train_times[i] = {stop - start}
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    plt.figure()
+
+    plt.title("svm 'linear', 'poly', 'rbf', 'sigmoid' ")
+    plt.legend(loc="best")
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    plt.gca().invert_yaxis()
+    # box-like grid
+    plt.grid()
+
+    # plot the std deviation as a transparent range at each training set size
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
+                     color="g")
+
+    # plot the average training and test score lines at each training set size
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+
+    # sizes the window for readability and displays the plot
+    # shows error from 0 to 1.1
+    plt.ylim(-.1, 1.1)
+    plt.show()
+
+
+
+#########################################################
+
 # plot scores
 plt.bar(
     x=range(len(scores_test)),
@@ -202,7 +452,16 @@ plt.xticks(range(len(scores_test)), list(scores_test.keys()))
 plt.ylabel('Confidence Score')
 
 plt.show()
-
+# # plot time
+# plt.bar(
+#     x=range(len(scores_train_times)),
+#     height=list(scores_train_times.values()),
+#     align='center',
+# )
+# plt.xticks(range(len(scores_train_times)), list(scores_train_times.keys()))
+# plt.ylabel('run times')
+#
+# plt.show()
 
 # plot learning curve
 def plot_learning_curve(estimator, estimator_title):
@@ -235,4 +494,7 @@ plot_learning_curve(dtree, "Decision Tree Classifier")
 plot_learning_curve(mlp, "MLP Classifier")
 plot_learning_curve(svm, "SVM Classifier")
 plot_learning_curve(gboost, "Gradient Boosting Classifier")
-plot_learning_curve(knn, "K-Neighbors Classifier")
+plot_learning_curve(knn, "K_5-Neighbors Classifier")
+plot_learning_curve(knn_3, "K_3-Neighbors Classifier")
+plot_learning_curve(knn_10, "K_10-Neighbors Classifier")
+print(scores_train_times)
